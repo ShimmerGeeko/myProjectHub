e2138695eb0f2bbb1c26223841159e4fe1ca5eeb
benazir <benazir.m@lendenclub.com>
2020-07-25 22:27:41 +0530

    code modified

--- src/App.js
+++ src/App.js
@@ -2,14 +2,61 @@ import React from 'react';
 //import {Link} from 'react-router';
 import './App.css';
 import Data from './data.js';
+import axios from 'axios';
+const cors = require('cors');
 class App extends React.Component {
-constructor()
-{
-super();
-this.state={ data: {} }
-}
-componentWillMount() {
-// fetch("https://gj2s6zgiq8.execute-api.us-west-2.amazonaws.com/v1/test-dynamo-info")
+ constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      greeting: '',
+      data:{}
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
+      .then(response => response.json())
+      .then(state => this.setState(state));
+  }
+
+fetchPosts() {
+  // fetch('https://gj2s6zgiq8.execute-api.us-west-2.amazonaws.com/v1/test-dynamo-info',
+  // {   
+  //   method:'GET',
+  //     mode: 'no-cors',
+  //     headers:{
+  //       "content-type": "application/json", 
+  //       "Access-Control-Allow-Methods": "OPTIONS,GET", 
+  //       "Access-Control-Allow-Origin": "*", 
+  //       "Access-Control-Allow-Header": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token" }
+  //     ,
+  // })
+  // .then(response=> response.json())
+  // .then(json => {
+     
+  //     this.setState({
+  //         data:json
+  //     }, function(){
+
+  //     });
+  // })
+  // .catch((error) =>{
+  //     console.error(error);
+  // });
+  // console.log(this.state.data,"test");
+  fetch(`https://gj2s6zgiq8.execute-api.us-west-2.amazonaws.com/v1/test-dynamo-info`)
+  .then(response => response.json())
+  .then(state => this.setState(state));
+
+  // fetch("https://gj2s6zgiq8.execute-api.us-west-2.amazonaws.com/v1/test-dynamo-info")
 // .then(response => response.json())
 // .then(json =>{
 //   if (json.code === 200) {
@@ -19,6 +66,32 @@ componentWillMount() {
 // }
 // console.log(this.state.date,"Hello")})
 // console.log("Data",this.state.data)
+
+
+}
+// componentDidMount() {
+//   const apiUrl = 'https://api.github.com/users/hacktivist123/repos';
+//   fetch(apiUrl)
+//     .then((response) => response.json())
+//     .then((data) => console.log('This is your data', data[0].id));
+
+//     axios({
+//       method: 'get',
+//       url: 'https://gj2s6zgiq8.execute-api.us-west-2.amazonaws.com/v1/test-dynamo-info',
+//     });
+// }
+componentDidMount() {
+  this.fetchPosts();
+//  fetch("gj2s6zgiq8.execute-api.us-west-2.amazonaws.com/v1/test-dynamo-info")
+//  .then(response => response.json())
+//  .then(json =>{
+//    if (json.code === 200) {
+//      this.setState({
+//      data: json
+//      });
+//  }
+//  console.log(this.state.date,"Hello")})
+//  console.log("Data",this.state.data)
 
 
 // fetch('gj2s6zgiq8.execute-api.us-west-2.amazonaws.com/v1/test-dynamo-info')
@@ -65,43 +138,54 @@ componentWillMount() {
 //   });
 
 
-fetch('https://gj2s6zgiq8.execute-api.us-west-2.amazonaws.com/v1/test-dynamo-info',
-        {   method:'GET',
-            mode: 'no-cors',
-            headers:{
-                'Access-Control-Allow-Origin':'*',
-                'Access-Control-Allow-Headers': "*"
-            },
-        })
-        .then((response) => response.json())
-        .then(json => {
-            console.log(json,"test");
-            this.setState({
-                data:json
-            }, function(){
+// fetch('https://gj2s6zgiq8.execute-api.us-west-2.amazonaws.com/v1/test-dynamo-info',
+//         {   method:'GET',
+//             mode: 'no-cors',
+//             headers:{
+//                 'Access-Control-Allow-Origin':'*',
+//                 'Access-Control-Allow-Headers': "*"
+//             },
+//         })
+//         .then((response) => response.json())
+//         .then(json => {
+//             console.log(json,"test");
+//             this.setState({
+//                 data:json
+//             }, function(){
 
-            });
-        })
-        .catch((error) =>{
-            console.error(error);
-        });
+//             });
+//         })
+//         .catch((error) =>{
+//             console.error(error);
+//         });
 }
-sendValue = (item) => {
-//console.log(this.state.userID);
-var str=item;
-console.log(str);
-var str1=str.substring(0,str.length-1);
-console.log(str1);
-var no=str1.lastIndexOf('/');
-console.log(no);
-var lnkno=str.substring(no+1,str.length);
-// this.props.router.push({pathname:'/data/'+lnkno,state:{values:str}});
-console.log('item', item, 'lnkno', lnkno)
-this.props.router.push('data/'+lnkno)
-}
+// sendValue = (item) => {
+// //console.log(this.state.userID);
+// var str=item;
+// console.log(str);
+// var str1=str.substring(0,str.length-1);
+// console.log(str1);
+// var no=str1.lastIndexOf('/');
+// console.log(no);
+// var lnkno=str.substring(no+1,str.length);
+// // this.props.router.push({pathname:'/data/'+lnkno,state:{values:str}});
+// console.log('item', item, 'lnkno', lnkno)
+// this.props.router.push('data/'+lnkno)
+// }
 render() {
   return (
 <div>
+<form onSubmit={this.handleSubmit}>
+            <label htmlFor="name">Enter your name: </label>
+            <input
+              id="name"
+              type="text"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+            <button type="submit">Submit</button>
+          </form>
+          <p>{this.state.greeting}</p>
   {console.log("Body",this.state.data)}
 
  <table>
