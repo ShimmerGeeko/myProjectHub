9fbf4c3108a66c82a47ec549d723db3a3c21321d
benazir <benazir.m@lendenclub.com>
2020-07-26 00:34:53 +0530

    modified

--- src/SOME.js
+++ src/SOME.js
@@ -1,19 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-// import './index.css';
-// import Data from './data'
-// import {Router,Route,hashHistory} from 'react-router';
-// ReactDOM.render(
-// // <App />,
-// <Router history ={hashHistory} >
-// <Route path="/"component={Data} />
-// <Route path="data/:id" component={App} />
-// {/* <Route path="location" component={Location}/> */}
-// </Router>,
-
-// document.getElementById('root')
-// );
 import React, { useState, useEffect } from 'react';
 
 function useApi(query) {
